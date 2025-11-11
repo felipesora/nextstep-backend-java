989797a7ea7fc85a8d2d813e8f1070c8b7a87941
felipesora <felipeusora@gmail.com>
2025-11-10 21:44:29 -0300

    wip: criando service, dto e mapper de Usuario

--- src/main/java/br/com/nextstep/repository/UsuarioRepository.java
+++ src/main/java/br/com/nextstep/repository/UsuarioRepository.java
@@ -2,11 +2,12 @@ package br.com.nextstep.repository;
 
 import br.com.nextstep.model.Usuario;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
-import java.awt.print.Pageable;
-
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Page<Usuario> findAllByOrderByIdAsc(Pageable pageable);
 
---
+++ src/main/java/br/com/nextstep/service/UsuarioService.java
@@ -0,0 +1,72 @@
+package br.com.nextstep.service;
+
+import br.com.nextstep.dto.usuario.UsuarioRequestDTO;
+import br.com.nextstep.dto.usuario.UsuarioResponseDTO;
+import br.com.nextstep.mapper.UsuarioMapper;
+import br.com.nextstep.model.Usuario;
+import br.com.nextstep.repository.UsuarioRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Transactional(readOnly = true)
+    public Page<UsuarioResponseDTO> listarTodos(Pageable pageable) {
+        return usuarioRepository.findAllByOrderByIdAsc(pageable)
+                .map(UsuarioMapper::toResponseDTO);
+    }
+
+    @Transactional(readOnly = true)
+    public UsuarioResponseDTO buscarPorId(Long id) {
+        var usuario = buscarEntidadeUsuarioPorId(id);
+        return UsuarioMapper.toResponseDTO(usuario);
+    }
+
+    @Transactional
+    public UsuarioResponseDTO salvar(UsuarioRequestDTO usuarioRequestDTO) {
+        var usuario = UsuarioMapper.toEntity(usuarioRequestDTO);
+
+        usuario.setSenha(passwordEncoder.encode(usuarioRequestDTO.getSenha()));
+
+        var usuarioSalvo = usuarioRepository.save(usuario);
+        return UsuarioMapper.toResponseDTO(usuarioSalvo);
+    }
+
+    @Transactional
+    public UsuarioResponseDTO atualizar(Long id, UsuarioRequestDTO usuarioRequestDTO) {
+        var usuarioAtual = buscarEntidadeUsuarioPorId(id);
+
+        usuarioAtual.setId(id);
+        usuarioAtual.setNome(usuarioRequestDTO.getNome());
+        usuarioAtual.setEmail(usuarioRequestDTO.getEmail());
+        if (usuarioRequestDTO.getSenha() != null && !usuarioRequestDTO.getSenha().isBlank()) {
+            usuarioAtual.setSenha(passwordEncoder.encode(usuarioRequestDTO.getSenha()));
+        }
+
+        var usuarioAtualizado = usuarioRepository.save(usuarioAtual);
+        return UsuarioMapper.toResponseDTO(usuarioAtualizado);
+    }
+
+    @Transactional
+    public void deletar(Long id) {
+        var usuario = buscarEntidadeUsuarioPorId(id);
+        usuarioRepository.delete(usuario);
+    }
+
+    private Usuario buscarEntidadeUsuarioPorId(Long id) {
+        return usuarioRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Usuário com id: " + id + " não encontrado"));
+    }
+}
